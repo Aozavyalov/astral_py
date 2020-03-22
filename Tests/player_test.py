621ba351ce664924afa2c9ccf164c1a18e7f0de5
Завьялов Андрей <aozavyalov@protonmail.com>
2020-03-22 20:54:45 +0300

    Added not completed tests and updated gitignore

---
+++ Tests/player_test.py
@@ -0,0 +1,56 @@
+import unittest
+
+from Classes.Player import Player
+
+class PlayerTest(unittest.TestCase):
+    def test_init(self):
+        with self.assertRaises(TypeError):
+            Player()
+        p = Player("lol")
+        self.assertEqual(p._name, "lol", f"Wrong name {p._name}!")
+        self.assertEqual(p._health, 30)
+        self.assertEqual(p._mana, 30)
+        self.assertEqual(p._max_health, 30)
+        self.assertEqual(p._max_mana, 30)
+        self.assertEqual(p._armor, 0)
+        self.assertEqual(p._mana_regen, 1)
+        self.assertEqual(p._damage_over_time, 0)
+        with self.assertRaises(ValueError):
+            Player("lol")
+        Player.remove_name("lol")
+        p = Player("lol", max_health=1, max_mana=1)
+        self.assertEqual(p._health, 1)
+        self.assertEqual(p._mana, 1)
+        self.assertEqual(p._max_health, 1)
+        self.assertEqual(p._max_mana, 1)
+        Player.remove_name("lol")
+        p = Player("lol", health=1, mana=1)
+        self.assertEqual(p._health, 1)
+        self.assertEqual(p._mana, 1)
+        Player.remove_name("lol")
+
+    def test_healing(self):
+        pass
+
+    def test_damaging(self):
+        pass
+
+    def test_next_round(self):
+        pass
+
+    def test_armor(self):
+        pass
+
+    def test_mana(self):
+        pass
+
+    def test_kill(self):
+        p = Player("lol")
+        self.assertTrue(p.is_alive)
+        p.sub_max_health(5)
+        p.kill()
+        self.assertFalse(p.is_alive)
+        p.reborn(30)
+        self.assertTrue(p.is_alive)
+        self.assertEqual(p._health, 30)
+        self.assertEqual(p._max_health, 30)
---
+++ test_all.py
@@ -0,0 +1,6 @@
+import unittest
+
+from Tests.player_test import PlayerTest
+
+if __name__ == "__main__":
+    unittest.main()
