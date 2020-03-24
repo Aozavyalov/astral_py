a0ec00bfe52336faee28cd538580a276a9496bdd
Завьялов Андрей <aozavyalov@protonmail.com>
2020-03-24 23:17:28 +0300

    Added move input

--- Classes/StandartIO.py
+++ Classes/StandartIO.py
@@ -1,6 +1,17 @@
 class StandartIO:
     def input(self):
-        pass
+        return input()
 
     def output(self):
         pass
+
+    def get_move(self):
+        move_str = self.input() # must be like "caster spell target"
+        return self.parse_move(move_str)
+
+    def parse_move(self, move_str):
+        move_str_parts = move_str.split()
+        if len(move_str_parts) != 2 and len(move_str_parts) != 3 and not isinstance(move_str_parts[1], int):
+            raise ValueError(f"Wrong move input!")
+        return {move_str_parts[0]: {"spell": int(move_str_parts[1]),
+                                    "target": None if len(move_str_parts) != 3 else move_str_parts[2]}}
