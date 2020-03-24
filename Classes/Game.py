4ef87789524ad3f8e2395cda5aa3d00724c6224b
Завьялов Андрей <aozavyalov@protonmail.com>
2020-03-24 19:02:34 +0300

    Added StandatIO and comments

--- Classes/Game.py
+++ Classes/Game.py
@@ -1,21 +1,29 @@
 import logging
 
 from Classes.Team import Team
+from Classes.StandartIO import StandartIO
 
 class Game:
-    def __init__(self, teams_num=2, players_in_team=1, start_hp=30, start_mp=30, rounds_num=30):
+    def __init__(self, teams_num=2, players_in_team=1,
+                 start_hp=30, start_mp=30, rounds_num=30,
+                 io_cls=StandartIO):
         self._logger = logging.getLogger()
         self._teams = create_teams(teams_num, players_in_team, start_hp, start_mp)
         self._rounds_num = rounds_num
+        self._io = io_cls()  # an object for input and output. Can be changed
 
     def create_teams(teams_num, players_in_team, start_hp, start_mp):
+        # teams is a set with objects Team
         teams = set()
         for i in range(teams_num):
+            # getting a name of a team. If pass it, it will be "Team №"
             team_name = input(f"Input team's name or it will be a number:")
             team_name = f"Team {i+1}" if not team_name else team_name
+            # getting names for players from this team
             players_names = set()
             for _ in range(players_in_team):
                 players_names.add(input("Input player's name:"))
+            # create and save a new team
             teams.add(Team(team_name, players_names, start_hp, start_mp))
         return teams
 
