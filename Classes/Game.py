386bbdc55e65056c168c39740f762378e8d62f32
Завьялов Андрей <aozavyalov@protonmail.com>
2020-04-05 00:50:08 +0300

    Updated teams creating

--- Classes/Game.py
+++ Classes/Game.py
@@ -7,26 +7,25 @@ from Classes.Team import Team
 
 class Game:
     def __init__(self, teams_num=2, players_in_team=1,
-                 start_hp=30, start_mp=30, rounds_num=30,
+                 start_hp=30, start_mp=30, max_hp=30, rounds_num=30,
                  io_cls=StandartIO):
         self._logger = logging.getLogger()
-        self._teams = create_teams(teams_num, players_in_team, start_hp, start_mp)
-        self._rounds_num = rounds_num
         self._io = io_cls()  # an object for input and output. Can be changed
+        self._teams = self.create_teams(teams_num, players_in_team, start_hp, max_hp, start_mp)
+        self._rounds_num = rounds_num
 
-    def create_teams(teams_num, players_in_team, start_hp, start_mp):
-        # teams is a set with objects Team
-        teams = set()
+    def create_teams(self, teams_num: int, players_in_team: int, start_hp: int, max_hp: int, start_mp: int):
+        teams = set()  # teams is a set with objects Team
         for i in range(teams_num):
             # getting a name of a team. If pass it, it will be "Team №"
-            team_name = input(f"Input team's name or it will be a number:")
+            team_name = self._io.input(f"Input team's name or it will be a number:")
             team_name = f"Team {i+1}" if not team_name else team_name
             # getting names for players from this team
             players_names = set()
             for _ in range(players_in_team):
                 players_names.add(input("Input player's name:"))
             # create and save a new team
-            teams.add(Team(team_name, players_names, start_hp, start_mp))
+            teams.add(Team.create_team(team_name, players_names, start_hp, max_hp, start_mp))
         return teams
 
     def start(self):
